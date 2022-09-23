118151345ca7625976885db59a42f5a209b74acc
PavithranBaskaran <vktpavi@gmail.com>
2022-09-23 19:46:30 +0530

    trying google analytics

--- src/App.js
+++ src/App.js
@@ -5,17 +5,26 @@ import Navbar from "./Components/Navbar/Navbar";
 import Specialization from "./Components/Specialization/Specialization";
 import Fabrics from "./Components/Fabrics/Fabrics";
 import Aboutus from "./Components/AboutUs/Aboutus";
+import Reactga from "react-ga";
+import { useEffect } from "react";
+
+const TRACKING_ID = "G-FXLW85GX4L";
+Reactga.initialize(TRACKING_ID);
 
 
 function App() {
+  useEffect(() => {
+    Reactga.pageview(window.location.pathname);
+  }, []);
+  
   return (
     <>
       <Navbar />
       <Fabrics />
       <Specialization />
       {/* <Application /> */}
-     
-      <Aboutus/>
+
+      <Aboutus />
       <Contact />
     </>
   );
